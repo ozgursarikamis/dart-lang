8e56005416d51276f5bf352e4ee060ac38cc3d61
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 03:52:12 +0300

    mixins

--- main.dart
+++ main.dart
@@ -1,5 +1,39 @@
 void main() {
-  var numbers = [1, 2, 3, 4, 5];
-  Iterable<int> output = numbers.map((e) => e * 2);
-  print(output);
+  Crocodile().hunt('zebra');
+  Alligator().hunt('fish');
+  Fish().feed();
+}
+
+mixin Swim {
+  void swim() => print('Swimming');
+}
+
+mixin Bite {
+  void bite() => print('Chomp');
+}
+
+mixin Crawl {
+  void crawl() => print('Crawling');
+}
+
+abstract class Reptile with Swim, Crawl, Bite {
+  void hunt(food) {
+    print('${this.runtimeType}----');
+    swim();
+    crawl();
+    bite();
+    print('Eat $food');
+  }
+}
+
+class Alligator extends Reptile {}
+
+class Crocodile extends Reptile {}
+
+class Fish with Swim, Bite {
+  void feed() {
+    print('----- fish -----');
+    swim();
+    bite();
+  }
 }
