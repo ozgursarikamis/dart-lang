4437f6aac6233a17f8ced96c44f1c60ec5909519
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 14:55:14 +0300

    maps

--- main.dart
+++ main.dart
@@ -1,37 +1,7 @@
 main() {
-  print([1, 3, 4].sum());
-  if ('pizzza'.isValidName) {
-    print('pizza is valid');
+  var myMap = <String, int>{'one': 1};
+  for (var item in myMap.keys) {
+    print(item);
   }
-
-  if ('pizza'.isValid()) {
-    print('pizza is valid again.');
-  }
-
-  // to prevent confusion with other libraries methods:
-  print(ExtendedString('pizza').isValidName);
-  print(ExtendedString('pizza').isValid());
-}
-
-extension ExtendedString on String {
-  bool get isValidName {
-    return !this.contains(new RegExp(r'[0-9]'));
-  }
-
-  isValid() {
-    return this.contains(new RegExp(r'[a-z]'));
-  }
-}
-
-extension IterableNumX<T extends num> on Iterable<T> {
-  T sum() {
-    var sum = (T == int ? 0 : 0.0) as T;
-
-    for (var current in this) {
-      if (current != null) {
-        sum += current;
-      }
-    }
-    return sum;
-  }
+  print(myMap);
 }
