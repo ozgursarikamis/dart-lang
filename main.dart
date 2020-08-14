9a7715b1239030b6003670d2306763f1a6e02f7d
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 03:27:58 +0300

    some iterable functions

--- main.dart
+++ main.dart
@@ -1,13 +1,27 @@
-void main() {
-  /**
-   * The difference with a List is that with the Iterable, 
-   * you can’t guarantee that reading elements by index will 
-   * be efficient. Iterable, as opposed to List, 
-   * doesn’t have the [] operator.
-   */
+bool predicate(String element) {
+  return element.length > 5;
+}
 
-  Iterable<int> iterable = [1, 2, 3];
-  // int value = iterable[1]; // Invalid
-  int value = iterable.elementAt(1);
-  print(value);
+void main() {
+  var items = ['Salad', 'Popcorn', 'Toast', 'Lasagne'];
+
+  var element1 = items.firstWhere((element) => element.length > 5);
+  print(element1);
+
+  var element2 = items.firstWhere(predicate);
+  print(element2);
+
+  var element3 =
+      items.firstWhere((element) => element.length > 10, orElse: () => 'None');
+  print(element3);
+
+  var element4 = items.singleWhere((element) => element == 'Popcorn');
+  print(element4);
+
+  if (items.any((element) => element == 'Salad')) {
+    print('we have a salad');
+  }
+  if (items.any((element) => element.contains('Salad'))) {
+    print('we have a salad');
+  }
 }
