1f85d5e314b13193d22b1be1246bd628d943fa63
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 14:30:46 +0300

    extension method

--- main.dart
+++ main.dart
@@ -1,13 +1,37 @@
-import 'dart:io';
+main() {
+  print([1, 3, 4].sum());
+  if ('pizzza'.isValidName) {
+    print('pizza is valid');
+  }
 
-Future<void> main() async {
-  try {
-    var description = await File('path').readAsString();
-    print(description);
-  } on IOException catch (e) {
-    print('file not found');
-    print(e);
-  } finally {
-    // clear some disposable.
+  if ('pizza'.isValid()) {
+    print('pizza is valid again.');
+  }
+
+  // to prevent confusion with other libraries methods:
+  print(ExtendedString('pizza').isValidName);
+  print(ExtendedString('pizza').isValid());
+}
+
+extension ExtendedString on String {
+  bool get isValidName {
+    return !this.contains(new RegExp(r'[0-9]'));
+  }
+
+  isValid() {
+    return this.contains(new RegExp(r'[a-z]'));
+  }
+}
+
+extension IterableNumX<T extends num> on Iterable<T> {
+  T sum() {
+    var sum = (T == int ? 0 : 0.0) as T;
+
+    for (var current in this) {
+      if (current != null) {
+        sum += current;
+      }
+    }
+    return sum;
   }
 }
