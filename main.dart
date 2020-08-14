73762bd0b9fcf98f369fce06076384d8a4952000
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 04:05:06 +0300

    try-catch

--- main.dart
+++ main.dart
@@ -1,10 +1,15 @@
 void main() {
   Iterable numbers = [1, 2, 3, 4, 5];
   for (var item in numbers) {
-    if (item > 4) {
-      throw ArgumentError('the number is greater than 4');
-    } else {
-      print(item);
+    try {
+      if (item > 4) {
+        throw Exception('the number is greater than 4');
+      } else {
+        print(item);
+      }
+    } catch (e) {
+      print(e.message);
+      print(e.errorName);
     }
   }
 }
