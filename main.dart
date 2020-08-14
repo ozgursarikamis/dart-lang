07f470de0662c787c6835be85d6366cbc4f7948b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 03:39:52 +0300

    mapping the Iterable

--- main.dart
+++ main.dart
@@ -1,12 +1,5 @@
-bool predicate(String element) {
-  return element.length > 5;
-}
-
 void main() {
-  var numbers = [1, 3, -2, 0, 4, 5];
-  var numbersUntilZero = numbers.takeWhile((value) => value != 0);
-  print('Numbers until zero $numbersUntilZero');
-
-  var numbersAfterZero = numbers.skipWhile((value) => value != 0);
-  print('Numbers until zero $numbersAfterZero');
+  var numbers = [1, 2, 3, 4, 5];
+  Iterable<int> output = numbers.map((e) => e * 2);
+  print(output);
 }
