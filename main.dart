4429e93a3b8c41ba197b9aa46da881365c0b4ce6
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-14 15:06:15 +0300

    Execution within async functions

--- main.dart
+++ main.dart
@@ -1,13 +1,20 @@
-main() {
-  print(createOrderMessage());
+Future<void> main() async {
+  countSeconds(3);
+  await printOrderMessage();
 }
 
-String createOrderMessage() {
-  fetchUserOrder();
-  return 'Your order is...';
+Future<void> printOrderMessage() async {
+  print('awiting user order');
+  var order = await fetchUserOrder();
+  print('Your order is $order');
 }
 
-Future<void> fetchUserOrder() {
-  return Future.delayed(
-      Duration(seconds: 2), () => throw new Exception('Fetching order failed'));
+Future<String> fetchUserOrder() {
+  return Future.delayed(Duration(seconds: 3), () => 'Large Latte');
+}
+
+void countSeconds(int s) {
+  for (var i = 0; i < s; i++) {
+    Future.delayed(Duration(seconds: i), () => print(i));
+  }
 }
