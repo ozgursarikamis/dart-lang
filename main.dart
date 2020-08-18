7007761e95af90718da2e88f56f2680210a76836
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-19 02:20:58 +0300

    dart http request

--- main.dart
+++ main.dart
@@ -1,20 +1,43 @@
-Future<void> main() async {
-  countSeconds(3);
-  await printOrderMessage();
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String url = "https://jsonplaceholder.typicode.com/albums/1";
+
+Future main(List<String> args) async {
+  // var response = await http.get(url);
+  // if (response.statusCode == HttpStatus.notFound) {
+  //   print("not found");
+  // } else {
+  //   print(response.contentLength);
+  // }
+  // print(response.statusCode);
+  var albums = await fetchAlbum();
+  print(albums.id);
+  print(albums.title);
+  print(albums.userId);
 }
 
-Future<void> printOrderMessage() async {
-  print('awiting user order');
-  var order = await fetchUserOrder();
-  print('Your order is $order');
+// Future<http.Response> fetchAlbum() {
+//   return http.get(url);
+// }
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return Album.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
 }
 
-Future<String> fetchUserOrder() {
-  return Future.delayed(Duration(seconds: 3), () => 'Large Latte');
-}
+class Album {
+  final int userId;
+  final int id;
+  final String title;
 
-void countSeconds(int s) {
-  for (var i = 0; i < s; i++) {
-    Future.delayed(Duration(seconds: i), () => print(i));
+  Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(userId: json['userId'], id: json['id'], title: json['title']);
   }
 }
